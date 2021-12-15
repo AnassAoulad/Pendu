e12952e423d32e9a6182787869143c95bbec9c61
AnassAoulad <anass.aoulad.bh@gmail.com>
2021-12-15 19:03:27 +0100

    adding dark mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, {
- useEffect, useState
-} from 'react'
+import React, { useContext } from 'react'
 import Input from "./input";
 import Classement from "./classement";
 import Word from "./word";
-import axios from 'axios';
-import { map } from 'lodash';
-/// import { API_URL } from '../settings';
+import { ThemeContext } from "./dark-mode";
+import SwitchButton from './button';
+import "./App.css"
 
 function App() {
-  return(
-    <div className="App">
-      <h1>Pendu</h1>
-        <React.Fragment>
-          <Word/>
-          <Input/>
-          <Classement/>
-      </React.Fragment>
+
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div className={`bg ${darkMode ? "bg-dark" : "bg-light"}`}>
+      <h1 className={`heading ${darkMode ? "heading-dark" : "heading-light"}`}>Pendu</h1>
+        <div className={`para ${darkMode ? "para-dark" : "para-light"}`}>
+          <React.Fragment>
+            <SwitchButton />
+            <Word/>
+            <Input/>
+            <Classement/>
+        </React.Fragment>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/button/index.js
+++ src/button/index.js
@@ -1,12 +1,22 @@
-import React from "react";
-import "./index.css";
+import React, { useContext } from "react";
+import { ThemeContext } from "../dark-mode";
 
-function Button({ onClick: handleClick, value = "anouar" }) {
+
+export default function SwitchButton() {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+  const onClick = () => {
+    if (darkMode) {
+      theme.dispatch({ type: "LIGHTMODE" });
+    } else {
+      theme.dispatch({ type: "DARKMODE" });
+    }
+  };
+
   return (
-    <button className="buttoncss" onClick={handleClick}>
-      {value}
+    <button className={`btn ${darkMode ? "btn-dark" : "btn-light"}`} onClick={onClick}>
+      {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
     </button>
   );
 }
-
-export default Button;
