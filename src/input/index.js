65dfeef3e5ece144e4c9fb194bbe67c427a6b15c
AnassAoulad <anass.aoulad.bh@gmail.com>
2021-12-15 22:24:17 +0100

    save

--- src/input/index.js
+++ src/input/index.js
@@ -4,10 +4,6 @@ import '../input/index.css'
 
 function Input() {
 
-  function handleChange(event) {
-    let enter = event.target.value;
-  }
-
   return (
     <div className="SearchBar">
       <input type="text" minLength="0" maxLength="1" name="search" onChange={handleChange} className="Input"/>
